e71a32a93f0ab825869f21843bbc3391325bceaa
raonwglobal <raon.w.global@gmail.com>
2026-08-26 22:43:37 +0700

    feat: expose integrated plugin runtime API

--- src/paper_live/plugins/__init__.py
+++ src/paper_live/plugins/__init__.py
@@ -1,6 +1,14 @@
-"""Secure GitHub-sourced plugin lifecycle primitives."""
+"""Secure GitHub-sourced plugin lifecycle and runtime primitives."""
 
 from .manifest import PluginManifest, PermissionPolicy, load_manifest
 from .registry import PluginRegistry
+from .integration import PluginExecutionRequest, PluginRuntime
 
-__all__ = ["PluginManifest", "PermissionPolicy", "PluginRegistry", "load_manifest"]
+__all__ = [
+    "PluginManifest",
+    "PermissionPolicy",
+    "PluginRegistry",
+    "PluginExecutionRequest",
+    "PluginRuntime",
+    "load_manifest",
+]
